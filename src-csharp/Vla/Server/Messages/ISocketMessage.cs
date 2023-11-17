44e6701b475214a2a1db9e408a60b7b5d724be19
Lucas <somfic@gmail.com>
2023-11-17 02:05:04 +0100

    ⚙️  Added `WebMessage` message

--- src-csharp/Vla/Server/Messages/ISocketMessage.cs
+++ src-csharp/Vla/Server/Messages/ISocketMessage.cs
@@ -1,14 +1,19 @@
 ﻿using Newtonsoft.Json;
+using Vla.Nodes.Connection;
+using Vla.Nodes.Instance;
 using Vla.Nodes.Structure;
 using Vla.Nodes.Types;
+using Vla.Nodes.Web;
 
 namespace Vla.Server.Messages;
 
 public record SocketMessage
 {
-    public string Id => GetType().Name;
+    public string Id => GetType().Name.Replace("Message", string.Empty);
 }
 
 public record NodesStructureMessage(IEnumerable<NodeStructure> Nodes, IEnumerable<NodeTypeDefinition> Types) : SocketMessage;
+
+public record WebMessage(Web Web) : SocketMessage;
 public record RecogniserRecognisedPartialMessage(string Json) : SocketMessage;
 public record RecogniserRecognisedMessage(string Json) : SocketMessage;
