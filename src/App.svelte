3c568ed8dd87b20acf045659d7e1b675194bf899
Lucas <somfic@gmail.com>
2023-11-22 21:20:57 +0100

    🖼️  Added splashscreen on application initialisation

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { partialRecognition, recognition, startListening, messages } from "./lib/ws";
+    import { partialRecognition, recognition, startListening, messages, isReady } from "./lib/ws";
     import { structures } from "./lib/nodes";
     import Editor from "./components/Editor.svelte";
+    import Splashscreen from "./components/Splashscreen.svelte";
 
     onMount(() => {
         startListening();
     });
 </script>
+
+<Splashscreen />
 
 <Editor />
 
---
+++ src/components/Splashscreen.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    import { isReady } from "../lib/ws";
+    import { progress } from "../lib/ws";
+
+    let show = true;
+
+    isReady.subscribe((r) => {
+        if (r) {
+            setTimeout(() => {
+                show = false;
+            }, 1000);
+        } else {
+            show = true;
+        }
+    });
+</script>
+
+{#if show}
+    <div class="splashscreen-background" class:ready={$isReady}>
+        <div class="splashscreen">
+            <p class="label">{$progress.Label}</p>
+            <div class="progress">
+                <div class="progress-bar" style={`width: ${$progress.Percentage * 100}%`}></div>
+            </div>
+        </div>
+    </div>
+{/if}
+
+<style lang="scss">
+    @import "../theme.scss";
+
+    .splashscreen-background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 200;
+        background-color: rgba(0, 0, 0, 0.5);
+        backdrop-filter: blur(5px);
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: 500ms ease all;
+
+        &.ready {
+            pointer-events: none;
+            opacity: 0;
+        }
+    }
+
+    .splashscreen {
+        width: 300px;
+        height: 400px;
+        background-color: $background;
+        border: 2px solid $border-color;
+        border-radius: 10px;
+        box-shadow: 0px 0px 100px 0px rgba(0, 0, 0, 1);
+        padding: 20px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: end;
+
+        .label {
+            font-size: 1rem;
+            font-weight: bold;
+            margin: 0;
+            margin-bottom: 2rem;
+        }
+
+        .progress {
+            width: 100%;
+            height: 5px;
+            background-color: $background-dark;
+            border-radius: 10px;
+            overflow: hidden;
+
+            .progress-bar {
+                height: 100%;
+                background-color: $accent;
+                transition: 1ms ease all;
+            }
+        }
+    }
+</style>
