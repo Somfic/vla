0f9ffc77e958a20d6bff5f63aa697a48b4b12a57
Lucas <somfic@gmail.com>
2023-11-22 21:21:51 +0100

    🖼️  Handle `ReadyState` message

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -43,6 +43,10 @@ export function startListening() {
             case "Progress":
                 progress.set(data);
                 break;
+
+            case "ReadyState":
+                isReady.set(data["Ready"]);
+                break;
         }
     };
 
