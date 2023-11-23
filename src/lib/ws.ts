aadeb28fe4d953145b94392571a6178596a054a0
Lucas <somfic@gmail.com>
2023-11-23 01:17:55 +0100

    🖼️  Add `runWeb` function to `isReady` subscription

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -1,5 +1,5 @@
 import { writable, type Writable } from "svelte/store";
-import { types, structures, instances, connections, result, type WebResult } from "./nodes";
+import { types, structures, instances, connections, result, type WebResult, runWeb } from "./nodes";
 
 let ws: WebSocket = null as any;
 
@@ -10,20 +10,34 @@ export let recognition: Writable<string> = writable("");
 export let progress: Writable<Progress> = writable({ Percentage: 0, Label: "Initialising" });
 export let isReady: Writable<boolean> = writable(false);
 
+isReady.subscribe((r) => {
+    if (r) {
+        runWeb();
+    }
+});
+
 export function startListening() {
+    hasConnected.set(false);
+    instances.set([]);
+    connections.set([]);
+    result.set({} as WebResult);
+    progress.set({ Percentage: 0, Label: "Initialising" });
+    isReady.set(false);
+
     console.log("Connecting to websocket...");
     ws = new WebSocket("ws://127.0.0.1:55155");
 
     ws.onopen = () => {
         console.log("Connected");
         hasConnected.set(true);
+        sendMessage({ Id: "getweb" });
     };
 
     ws.onmessage = (e) => {
         messages.update((old) => [...old, e.data]);
 
         const data = JSON.parse(e.data);
-        console.log(">", data);
+        console.log(">", JSON.stringify(data));
 
         switch (data["Id"]) {
             case "NodesStructure":
@@ -56,7 +70,6 @@ export function startListening() {
 
     ws.onclose = (e) => {
         console.log("Closed", e);
-        hasConnected.set(false);
 
         setTimeout(() => {
             startListening();
