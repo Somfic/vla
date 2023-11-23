60f5f2211c5d06e9dab48dbc736ecb5e7d4ee1ad
Lucas <somfic@gmail.com>
2023-11-23 01:19:03 +0100

    🖼️  Removed unused logging

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { partialRecognition, recognition, startListening, messages, isReady } from "./lib/ws";
-    import { structures } from "./lib/nodes";
+    import { result, structures } from "./lib/nodes";
     import Editor from "./components/Editor.svelte";
     import Splashscreen from "./components/Splashscreen.svelte";
 
@@ -13,6 +13,3 @@
 <Splashscreen />
 
 <Editor />
-
-<p>{$recognition}</p>
-<p>{$partialRecognition}</p>
