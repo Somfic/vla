e1c33bcb6e68ef1bed4368ea306a044362fb609d
Lucas <somfic@gmail.com>
2023-12-03 23:27:51 +0100

    ⚙️  Add default values for Instances and Connections in Web struct

--- src-csharp/Vla/Nodes/Web/Web.cs
+++ src-csharp/Vla/Nodes/Web/Web.cs
@@ -6,9 +6,13 @@ namespace Vla.Nodes.Web;
 
 public readonly struct Web
 {
+	public Web()
+	{
+	}
+
 	[JsonProperty("instances")]
-	public NodeInstance[] Instances { get; init; }
+	public NodeInstance[] Instances { get; init; } = Array.Empty<NodeInstance>();
 	
 	[JsonProperty("connections")]
-	public NodeConnection[] Connections { get; init; }
+	public NodeConnection[] Connections { get; init; } = Array.Empty<NodeConnection>();
 }
