def9aa01ae1d3772bba6d57dc1b0b04dc75982cf
Lucas <somfic@gmail.com>
2023-12-05 23:49:01 +0100

    🖼️  Fixed formatting

--- src/components/EditorAnchorDefaultValue.svelte
+++ src/components/EditorAnchorDefaultValue.svelte
@@ -3,47 +3,46 @@
     import { types, type ParameterStructure, typeToDefinition, type ParameterInstance, type NodeStructure } from "../lib/nodes";
     import Value from "./Value.svelte";
 
-	export let structure: NodeStructure;
+    export let structure: NodeStructure;
     export let parameter: ParameterInstance;
     export let linked: boolean;
     export let hovering: boolean;
     export let connecting: boolean;
 
-	function handleClick(e: MouseEvent) {
-		//e.stopImmediatePropagation();
-		e.stopPropagation();
-		//e.preventDefault();
-	}
+    function handleClick(e: MouseEvent) {
+        //e.stopImmediatePropagation();
+        e.stopPropagation();
+        //e.preventDefault();
+    }
 
-	$: parameterType = structure.inputs.find((i) => i.id == parameter.id)?.type.replace("&", "")!;
+    $: parameterType = structure.inputs.find((i) => i.id == parameter.id)?.type.replace("&", "")!;
 </script>
 
 <div class="default-wrapper">
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<div class="default" class:linked class:hovering class:connecting on:mousedown={handleClick}>
-	<Value type={typeToDefinition(parameterType)} bind:value={parameter.value} />
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="default" class:linked class:hovering class:connecting on:mousedown={handleClick}>
+        <Value type={typeToDefinition(parameterType)} bind:value={parameter.value} input />
+    </div>
 </div>
-</div>
-
 
 <style lang="scss">
     @import "../theme.scss";
 
-	.default-wrapper {
+    .default-wrapper {
         position: absolute;
         top: -0.5rem;
-	}
+    }
 
     .default {
-		display: inline-block;
-		left: calc(-100% - 0.5rem);
-		position: relative;
-		opacity: 0.5;
-		transition: all ease 200ms;
+        display: inline-block;
+        left: calc(-100% - 0.5rem);
+        position: relative;
+        opacity: 0.5;
+        transition: all ease 200ms;
 
-		&:hover {
-			opacity: 1;
-		}
-	}
+        &:hover {
+            opacity: 1;
+        }
+    }
 </style>
--- src/components/Value.svelte
+++ src/components/Value.svelte
@@ -1,99 +1,100 @@
 <script lang="ts">
     import type { TypeDefinition } from "../lib/nodes";
-	export let type: TypeDefinition;
-	export let value: any;
-	export let readonly = false;
-	export let input = false;
-	export let output = false;
+    export let type: TypeDefinition;
+    export let value: any;
+    export let readonly = false;
+    export let input = false;
+    export let output = false;
 </script>
 
 <div class="value" class:hasValue={value != null} class:readonly class:input class:output>
-{#if type.htmlType == "text"}
-    <input {readonly} bind:value type="text" placeholder={type.defaultValue} />
-{:else if type.htmlType == "number" && !readonly}
-    <input {readonly} bind:value type="number" placeholder={type.defaultValue} />
-{:else if type.htmlType == "number" && readonly}
-	<input {readonly} bind:value type="text" placeholder={type.defaultValue} />
-{:else if type.htmlType == "select"}
-	{#if readonly}
-		<input {readonly} bind:value type="text" placeholder={type.defaultValue} />
-	{:else}
-		<select bind:value>
-			{#each type.values as option}
-				<option value={option.value}>{option.name}</option>
-			{/each}
-		</select>
-	{/if}
-{:else if type.htmlType == "checkbox"}
-    <input disabled={readonly} {readonly} bind:checked={value} type="checkbox" />
-{/if}
+    {#if type.htmlType == "text"}
+        <input {readonly} bind:value type="text" placeholder={type.defaultValue} />
+    {:else if type.htmlType == "number" && !readonly}
+        <input {readonly} bind:value type="number" placeholder={type.defaultValue} />
+    {:else if type.htmlType == "number" && readonly}
+        <input {readonly} bind:value type="text" placeholder={type.defaultValue} />
+    {:else if type.htmlType == "select"}
+        {#if readonly}
+            <input {readonly} bind:value type="text" placeholder={type.defaultValue} />
+        {:else}
+            <select bind:value>
+                {#each type.values as option}
+                    <option value={option.value}>{option.name}</option>
+                {/each}
+            </select>
+        {/if}
+    {:else if type.htmlType == "checkbox"}
+        <input disabled={readonly} {readonly} bind:checked={value} type="checkbox" />
+    {/if}
 </div>
 
 <style lang="scss">
-	@import "../theme.scss";
+    @import "../theme.scss";
 
-	.value {
-		display: flex;
-		transition: all ease 200ms;
+    .value {
+        display: flex;
+        transition: all ease 200ms;
 
-        &.input {
+        &.input input {
             text-align: left;
         }
 
         &.output input {
             text-align: right;
-			margin-right: 0.5rem;
+            margin-right: 0.5rem;
         }
 
-		&.readonly {
-			opacity: 0;
+        &.readonly {
+            opacity: 0;
         }
 
-		&.hasValue {
-			opacity: 1;
-		}
-
-        input, select {
-			all: unset;
-			cursor: pointer;
-			font-weight: bold;
-			font-size: 0.8rem;
-			height: 1rem;
-			flex-grow: 1;
-			width: 50px;
-			border: 2px solid $border-color;
-			border-radius: 10px;
-			background-color: $background-dark;
-			padding: 3px 6px;
-			color: #999999;
+        &.hasValue {
+            opacity: 1;
         }
 
-		input[type="checkbox"] { 
-			width: calc(1rem + 10px);
-			height: calc(1rem + 10px);
-			margin: 0;
-			padding: 0;
-			appearance: none;
-			background-color: $background-dark;
-			border: 2px solid $border-color;
-			border-radius: 10px;
-			transition: all ease 200ms;
-			cursor: pointer;
+        input,
+        select {
+            all: unset;
+            cursor: pointer;
+            font-weight: bold;
+            font-size: 0.8rem;
+            height: 1rem;
+            flex-grow: 1;
+            width: 50px;
+            border: 2px solid $border-color;
+            border-radius: 10px;
+            background-color: $background-dark;
+            padding: 3px 6px;
+            color: #999999;
+        }
 
-			&:checked {
-				background-color: $accent;
-				border-color: $accent;
-			}
-		}
+        input[type="checkbox"] {
+            width: calc(1rem + 10px);
+            height: calc(1rem + 10px);
+            margin: 0;
+            padding: 0;
+            appearance: none;
+            background-color: $background-dark;
+            border: 2px solid $border-color;
+            border-radius: 10px;
+            transition: all ease 200ms;
+            cursor: pointer;
 
-		input {
-			font-family: $font-mono;
-		}
+            &:checked {
+                background-color: $accent;
+                border-color: $accent;
+            }
+        }
 
-		select {
-			width: calc(100% + 5rem);
-			text-align: right;
-			padding-right: 1rem;
-		}
+        input {
+            font-family: $font-mono;
+        }
+
+        select {
+            width: calc(100% + 5rem);
+            text-align: right;
+            padding-right: 1rem;
+        }
     }
 </style>
