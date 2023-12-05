3d8396db760c76c17339d90008acc734cbf25131
Lucas <somfic@gmail.com>
2023-12-05 18:49:36 +0100

    🖼️  Apply a gradient. Fix #22

--- src/components/EditorEdge.svelte
+++ src/components/EditorEdge.svelte
@@ -1,17 +1,54 @@
 <script lang="ts">
-    import { Edge } from "svelvet";
+    import { Edge, type WritableEdge } from "svelvet";
+    import { instances, structures, type ParameterStructure, typeToDefinition } from "../lib/nodes";
+    import { get } from "svelte/store";
+    import { blendColors } from "../lib/color";
 
-	let ref: SVGPathElement | undefined = undefined;
+    let ref: SVGPathElement | undefined = undefined;
+    let edge: WritableEdge;
+
+    $: source = findParameter(edge?.source.id, false);
+    $: target = findParameter(edge?.target.id, true);
+
+    $: startColor = typeToDefinition(source?.type ?? "")?.color?.hex ?? "#ffffff";
+    $: midwayColor = blendColors(startColor, stopColor, 0.5);
+    $: stopColor = typeToDefinition(target?.type ?? "")?.color?.hex ?? "#000000";
+
+    function findParameter(id: string | null, isInput: boolean): ParameterStructure | undefined {
+        if (id == null) return undefined;
+
+        let parameterId = id.split("/")[0].substring(2); // remove "A-"
+        let instanceId = id.split("/")[1].substring(2); // remove "N-"
+
+        let instance = get(instances).find((i) => i.id == instanceId);
+        let structure = get(structures).find((s) => s.nodeType == instance?.nodeType);
+
+        if (isInput) {
+            return structure?.inputs.find((p) => p.id == parameterId);
+        } else {
+            return structure?.outputs.find((p) => p.id == parameterId);
+        }
+    }
 </script>
 
-<Edge let:path let:destroy edgeClick="{() => alert('Edge clicked')}" step>
-	<path bind:this={ref} d={path} />
+<Edge bind:edge let:path let:destroy edgeClick={() => alert("Edge clicked")}>
+    <!-- Define a gradient that goes from the source color to the target color -->
+    <defs>
+        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stop-color={startColor} />
+            <stop offset="50%" stop-color={midwayColor} />
+            <stop offset="100%" stop-color={stopColor} />
+        </linearGradient>
+    </defs>
+
+    <path bind:this={ref} d={path} />
 </Edge>
 
 <style lang="scss">
-	path {
-		stroke: #696969;
-		stroke-width: 3px;
-		z-index: 1;
-	}
+    path {
+        // Gradient fill based on the source and target colors
+        stroke: url(#gradient);
+        stroke-width: 20px;
+        z-index: 1;
+    }
 </style>
