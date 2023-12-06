820af06b9c7d3035e0aec778f2d0ed049a67a5d9
Lucas <somfic@gmail.com>
2023-12-06 17:57:53 +0100

    ⚙️  Added boolean logic operators. Fix #26

---
+++ src-csharp/Vla.Nodes/Booleans/OrBooleanNode.cs
@@ -0,0 +1,17 @@
+﻿using Vla.Abstractions;
+using Vla.Abstractions.Attributes;
+
+namespace Vla.Nodes.Booleans;
+
+[Node("OR operator")]
+[NodeCategory("Booleans")]
+[NodeTags("Or", "Operator", "||", "Logic", "Gate")]
+public class OrBooleanNode : INode
+{
+	public string Name => "OR";
+	
+	public void Execute([NodeInput("Left")] bool left, [NodeInput("Right")] bool right, [NodeOutput("Result")] out bool result)
+	{
+		result = left || right;
+	}
+}
