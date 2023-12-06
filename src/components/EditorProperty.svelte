ef66d469dc52eea33fabb1a6e026533d08fff246
Lucas <somfic@gmail.com>
2023-12-06 17:59:42 +0100

    🖼️  Removed unused css

--- src/components/EditorProperty.svelte
+++ src/components/EditorProperty.svelte
@@ -11,8 +11,8 @@
 </script>
 
 <div class="property">
-    <div class="property-name">{property.name}</div>
-    <Value {type} bind:value={value} />
+    <div class="name">{property.name}</div>
+    <Value {type} bind:value />
 </div>
 
 <style lang="scss">
@@ -25,46 +25,5 @@
 
         margin: 1rem 15px;
         gap: 6px;
-
-        input, select {
-            all: unset;
-            max-width: 80px;
-            text-align: right;
-            height: 1rem;
-            border: 2px solid #424242;
-            border-radius: 5px;
-            background-color: #1f1f1f;
-            padding: 5px;
-            color: #696969; // nice
-            transition: all ease 200ms;
-
-            &:focus {
-                border-color: $accent;
-                color: white;
-            }
-
-            &[type="checkbox"] {
-                width: 1rem;
-                height: 1rem;
-                margin: 0;
-                padding: 0;
-                appearance: none;
-                background-color: #1f1f1f;
-                border: 2px solid #424242;
-                border-radius: 5px;
-                transition: all ease 200ms;
-                cursor: pointer;
-
-                &:checked {
-                    background-color: $accent;
-                    border-color: $accent;
-                }
-            }
-        }
-
-        select {
-            padding-right: 1.1rem;
-            cursor: pointer;
-        }
     }
 </style>
