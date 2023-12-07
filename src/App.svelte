05ab229dcd06710b1ac34303c34d079ab166596e
Lucas <somfic@gmail.com>
2023-12-07 17:14:49 +0100

    🖼️  Added topbar component

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
     import { result, structures, types } from "./lib/nodes";
     import Editor from "./components/Editor.svelte";
     import Splashscreen from "./components/Splashscreen.svelte";
+    import Topbar from "./components/topbar/Topbar.svelte";
 
     onMount(() => {
         startListening();
@@ -12,4 +13,17 @@
 
 <Splashscreen />
 
+<div class="topbar">
+    <Topbar />
+</div>
 <Editor />
+
+<style lang="scss">
+    .topbar {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        z-index: 100;
+    }
+</style>
---
+++ src/components/topbar/Topbar.svelte
@@ -0,0 +1,33 @@
+<div class="topbar">
+    <div class="left"></div>
+    <div class="center">
+        <div class="name">
+            <p class="workspace">Drafts</p>
+            <div class="divider">/</div>
+            <p class="name">Autosave</p>
+        </div>
+    </div>
+    <div class="right"></div>
+</div>
+
+<style lang="scss">
+    @import "../../theme.scss";
+
+    .topbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 1rem;
+        height: 3rem;
+        background-color: $background-frosted;
+        backdrop-filter: $filter-frosted;
+        border-bottom: 1px solid $border-color;
+        color: $foreground;
+    }
+
+    .name {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+    }
+</style>
