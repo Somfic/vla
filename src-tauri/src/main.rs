932c71df54fa12b34f75f417bf6997c1634836c9
Lucas <somfic@gmail.com>
2023-12-12 13:16:51 +0100

    📦  Removed .NET

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,28 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 //#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::api::process::{Command, CommandEvent};
-
 fn main() {
-    let (mut rx, _) = Command::new_sidecar("csharp")
-        .expect("failed to create `csharp` binary command")
-        .spawn()
-        .expect("Failed to spawn sidecar for `csharp` binary");
-
-    tauri::async_runtime::spawn(async move {
-        while let Some(event) = rx.recv().await {
-            match event {
-                CommandEvent::Stdout(line) => {
-                    print!("CSHARP: {}", line);
-                }
-                CommandEvent::Stderr(line) => {
-                    print!("CSHARP: {}", line);
-                }
-                _ => {}
-            }
-        }
-    });
-
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
