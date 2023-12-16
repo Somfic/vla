1294b7bf53916fc9ffebe3edd718fb7363021103
Lucas <lucas@somfic.dev>
2023-12-16 23:19:26 +0100

    📦  Apply `mica` transparency frosting on Windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
-use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+use window_vibrancy::{apply_acrylic, apply_mica, apply_vibrancy, NSVisualEffectMaterial};
 
 fn main() {
     tauri::Builder::default()
@@ -14,8 +14,12 @@ fn main() {
                 .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
             #[cfg(target_os = "windows")]
-            apply_blur(&window, Some((18, 18, 18, 125)))
-                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+            {
+                println!("Applying mica");
+                //apply_mica(&window, None)
+                apply_acrylic(&window, Some((30, 30, 30, 255)))
+                    .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
+            }
 
             Ok(())
         })
