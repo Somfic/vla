28a5150a71dbaaf81df83d8b619ec8555803aafc
Lucas <lucas@somfic.dev>
2023-12-18 04:56:03 +0100

    📦  Run `csharp` sidecar when starting application

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+use tauri::{
+    api::process::{Command, CommandEvent},
+    Manager,
+};
 use window_vibrancy::{apply_acrylic, apply_mica, apply_vibrancy, NSVisualEffectMaterial};
 
 fn main() {
+    let (mut rx, _) = Command::new_sidecar("csharp")
+        .expect("failed to create `csharp` binary command")
+        .spawn()
+        .expect("Failed to spawn sidecar for `csharp` binary");
+
+    tauri::async_runtime::spawn(async move {
+        while let Some(event) = rx.recv().await {
+            match event {
+                CommandEvent::Stdout(line) => {
+                    print!("CSHARP: {}", line);
+                }
+                CommandEvent::Stderr(line) => {
+                    print!("CSHARP: {}", line);
+                }
+                _ => {}
+            }
+        }
+    });
+
     tauri::Builder::default()
         .setup(|app| {
             let window = app.get_window("main").unwrap();
