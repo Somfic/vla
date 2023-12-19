645bf1cf0e6dcadb175b5d3d21ff7cb5cdf01821
Lucas <lucas@somfic.dev>
2023-12-19 21:17:14 +0100

    ⚙️  Added documentation

--- src-csharp/Vla.Nodes/Instance/PropertyInstance.cs
+++ src-csharp/Vla.Nodes/Instance/PropertyInstance.cs
@@ -4,12 +4,21 @@ namespace Vla.Nodes.Instance;
 
 public readonly struct PropertyInstance(string name, Type type, string value)
 {
+    /// <summary>
+    /// The name of the property.
+    /// </summary>
     [JsonProperty("name")]
     public string Name { get; init; } = name;
 
+    /// <summary>
+    /// The type of the property.
+    /// </summary>
     [JsonProperty("type")]
     public Type Type { get; init; } = type;
 
+    /// <summary>
+    /// The JSON encoded value of the property.
+    /// </summary>
     [JsonProperty("value")]
     public string Value { get; init; } = value;
 }
