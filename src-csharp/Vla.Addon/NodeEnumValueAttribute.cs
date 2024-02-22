c990fcb79ac5556e49a29cbe4ce73c1f5ea345f9
Lucas <lucas@somfic.dev>
2024-02-22 18:26:45 +0100

    ⚙️  Added categories to `NodeEnumValue` attribute

--- src-csharp/Vla.Addon/NodeEnumValueAttribute.cs
+++ src-csharp/Vla.Addon/NodeEnumValueAttribute.cs
@@ -1,7 +1,9 @@
 namespace Vla.Addon;
 
 [AttributeUsage(AttributeTargets.Field)]
-public class NodeEnumValueAttribute(string name) : Attribute
+public class NodeEnumValueAttribute(string name, string category = "") : Attribute
 {
 	public string Name { get; } = name;
+	
+	public string Category { get; } = category;
 }
