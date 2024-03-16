ff6075b5a7cdb51089d16f08d2209925e5ff6ffb
Lucas <29230467+Somfic@users.noreply.github.com>
2024-03-16 13:19:51 +0100

    Added `WorkspaceService` tests

--- src-csharp/Tests/Vla.Tests/Server/WorkspaceService.cs
+++ src-csharp/Tests/Vla.Tests/Server/WorkspaceService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Somfic.Common;
 using Vla.Nodes;
 using Vla.Server.Messages.Requests;
 using Vla.Websocket;
@@ -12,7 +13,7 @@ namespace Vla.Tests.Server;
 
 public class WorkspaceService
 {
-	private const string WorkspacePath = "workspaces";
+	private static readonly string WorkspacePath = Path.GetFullPath(Path.Combine("workspaces"));
 
 	// Before runnning the tests, make sure to delete the workspaces folder (NUnit)
 	[OneTimeSetUp]
@@ -20,7 +21,7 @@ public class WorkspaceService
 	{
 		if (Directory.Exists(WorkspacePath))
 			Directory.Delete(WorkspacePath, true);
-		
+
 		Directory.CreateDirectory(WorkspacePath);
 	}
 	
@@ -31,22 +32,95 @@ public class WorkspaceService
 		var workspaces = CreateWorkspaceService();
 
 		var (id, path) = RandomIdAndPath();
-		
-		var result = await workspaces.CreateAsync(id, path);
-		
-		Console.WriteLine(result);
-		
-		Assert.That(result.IsValue, Is.True);
-		
-		result = await workspaces.CreateAsync(id, path);
-		
-		Assert.That(result.IsError, Is.True);
-	}
 
+		var workspace = (await workspaces.CreateAsync(id, path)).Expect();
+		
+		var workspaceResult = await workspaces.CreateAsync(id, path);
+		
+		Assert.That(workspaceResult.IsError, Is.True);
+	}
+	
+	[Test]
+	public async Task WorkspaceService_List_ReturnsWorkspaces()
+	{
+		var workspaces = CreateWorkspaceService();
+
+		var (id, path) = RandomIdAndPath();
+		
+		var workspace = (await workspaces.CreateAsync(id, path)).Expect();
+
+		var recents = (await workspaces.ListAsync()).Expect();
+		
+		Assert.That(recents.Any(x => x.Name == id), Is.True);
+	}
+	
+	[Test]
+	public async Task WorkSpaceService_Load_LoadsWorkspace()
+	{
+		var workspaces = CreateWorkspaceService();
+
+		var (id, path) = RandomIdAndPath();
+
+		(await workspaces.CreateAsync(id, path)).Expect();
+		var workspace = (await workspaces.LoadAsync(path)).Expect();
+		
+		Assert.That(workspace.Name, Is.EqualTo(id));
+	}
+	
+	[Test]
+	public async Task WorkspaceService_CreateWeb_AddsWeb()
+	{
+		var workspaces = CreateWorkspaceService();
+
+		var (id, path) = RandomIdAndPath();
+
+		var workspace = (await workspaces.CreateAsync(id, path)).Expect();
+		var web = (await workspaces.CreateWebAsync(workspace, "test web")).Expect();
+		
+		Assert.That(web.Name, Is.EqualTo("test web"));
+
+		workspace = (await workspaces.LoadAsync(path)).Expect();
+		Assert.That(workspace.Webs.Any(x => x.Name == "test web"), Is.True);
+	}
+	
+	[Test]
+	public async Task WorkspaceService_CreateOrLoad_CreatesOnNonExistent()
+	{
+		var workspaces = CreateWorkspaceService();
+
+		var (id, path) = RandomIdAndPath();
+
+		var oldList = (await workspaces.ListAsync()).Expect();
+
+		var workspace = (await workspaces.CreateOrLoadAsync(id, path)).Expect();
+		
+		var newList = (await workspaces.ListAsync()).Expect();
+
+		Assert.That(oldList.Any(x => x.Name == workspace.Name), Is.False);
+		Assert.That(newList.Any(x => x.Name == workspace.Name), Is.True);
+	}
+	
+	[Test]
+	public async Task WorkspaceService_CreateOrLoad_LoadsOnExistent()
+	{
+		var workspaces = CreateWorkspaceService();
+
+		var (id, path) = RandomIdAndPath();
+
+		var workspace1 = (await workspaces.CreateAsync(path)).Expect();
+		
+		var oldList = (await workspaces.ListAsync()).Expect();
+
+		var workspace2 = (await workspaces.CreateOrLoadAsync(id, path)).Expect();
+		
+		Assert.That(workspace1.Name, Is.EqualTo(workspace2.Name));
+	}
+	
+	
 	private (string id, string path) RandomIdAndPath()
 	{
 		var id = Guid.NewGuid().ToString();
-		var path = Path.Combine(WorkspacePath, id);
+		var path = Path.Combine(WorkspacePath, id + ".vla");
 		return (id, path);
 	}
     
