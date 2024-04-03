8a09c13bf0708b08ab016e0a423fe09487e19bf3
Lucas <29230467+Somfic@users.noreply.github.com>
2024-04-03 02:00:23 +0200

    Fixed state not updating live

--- src-client/src/lib/components/Node.svelte
+++ src-client/src/lib/components/Node.svelte
@@ -11,25 +11,23 @@
 		outputs: NamedValue[];
 		properties: NamedValue[];
 	};
-
-	const { inputs, outputs, properties } = data;
 </script>
 
 <div class="node">
 	<div class="title">{data.name}</div>
 	<div class="properties">
-		{#each properties as property}
+		{#each data.properties as property}
 			<div class="property">
 				{property.label}: {property.value}
 			</div>
 		{/each}
-		{#each inputs as input}
+		{#each data.inputs as input}
 			<div class="input">
 				<p>Input: {input.label} ({input.value})</p>
 				<Handle type="source" position={Position.Left} id={input.id} />
 			</div>
 		{/each}
-		{#each outputs as output}
+		{#each data.outputs as output}
 			<div class="output">
 				<p>Output: {output.label} ({output.value})</p>
 				<Handle type="target" position={Position.Right} id={output.id} />
