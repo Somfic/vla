83644ac0755efec6ccd91cec79d5d2c5974c3e6d
Lucas <lucas@somfic.dev>
2024-04-06 02:35:54 +0200

    ⚙️  Engine rewrite to work with branches

---
+++ src-csharp/Vla.Addon/OutgoingBranchAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Vla.Addon;
+
+/// <summary>
+///		This attribute is used to mark an outgoing branch of a node.
+/// </summary>
+/// <param name="name">The identifier of the branch.</param>
+/// <param name="label">The display name of the branch.</param>
+[AttributeUsage(AttributeTargets.Property)]
+public class OutgoingBranchAttribute(string name, string label) : Attribute
+{
+	public string Name { get; } = name;
+	
+	public string Label { get; } = label;
+}
