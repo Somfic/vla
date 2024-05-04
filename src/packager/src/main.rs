f20c80519fc0933831a5081cb49432b7a5972856
Lucas <lucas@somfic.dev>
2024-05-04 22:47:51 +0200

    disable unused variable on linux builds

--- src/packager/src/main.rs
+++ src/packager/src/main.rs
@@ -11,14 +11,12 @@ use window_vibrancy::apply_acrylic;
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let main_window = app.get_window("main").unwrap();
-
             #[cfg(target_os = "macos")]
-            apply_vibrancy(main_window, NSVisualEffectMaterial::HudWindow, None, None)
+            apply_vibrancy(app.get_window("main").unwrap(), NSVisualEffectMaterial::HudWindow, None, None)
                 .expect("Unsupported platform! Window vibrancy is only supported on macOS machines");
 
             #[cfg(target_os = "windows")]
-            apply_acrylic(main_window, None)
+            apply_acrylic(app.get_window("main").unwrap(), None)
                 .expect("Unsupported platform! Window vibrancy effect is only supported on Windows machines");
 
             Ok(())
