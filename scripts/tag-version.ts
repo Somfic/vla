911a67ffe2cbc8af85b1806d639fb5eec1528687
Lucas <lucas@somfic.dev>
2024-05-05 15:10:15 +0200

    rewrote `tag-version` script

--- scripts/tag-version.ts
+++ scripts/tag-version.ts
@@ -2,160 +2,136 @@ import { readFileSync, writeFileSync } from "fs";
 import { glob } from "glob";
 import child_process from "child_process";
 
-// Get command line argument for the version
-var version = "";
-var tag = "";
+let bumpType: "major" | "minor" | "patch" | "prerelease" = "minor";
 
 const branch = child_process.execSync("git branch --show-current", { encoding: "utf8" }).trim();
+const isPreRelease = branch === "next";
+const tags = child_process
+    .execSync("git ls-remote --tags --sort=-committerdate", { encoding: "utf8" })
+    .trim()
+    .split("\n")
+    .map((tag) => tag.split("/")[2])
+    .map((tag) => tag.replace(/^v/, ""));
+const author = child_process.execSync("git log -1", { encoding: "utf8" }).trim().split("\n")[1].replace("Author: ", "");
+const isCi = process.env.CI;
+const isMergeCommit = author.toLocaleLowerCase().includes("merge");
+const commitMessage = child_process.execSync("git log -1 --pretty=%B", { encoding: "utf8" }).trim();
+const currentVersion = tags.filter((tag) => !tag.includes("-next"))[0]?.replace(/^v/, "") ?? "0.0.0";
+const nextVersion = tags.filter((tag) => tag.includes("-next"))[0]?.split("-next")[0] + "-next" ?? "v0.1.0-next";
 
-if (process.argv.length <= 2) {
-    console.log("No version specified, attempting to determine from commit message");
-
-    var author = child_process.execSync("git log -1", { encoding: "utf8" }).trim().split("\n")[1].replace("Author: ", "");
-
-    if (branch == "main") {
-        if (author.toLocaleLowerCase().includes("merge")) {
-            console.log("Merging commit detected");
-            version = "minor";
-        } else {
-            version = "patch";
-        }
-    } else {
-        version = "minor";
-    }
+// Determine bump type
+if (isPreRelease) {
+    bumpType = "prerelease";
+} else if (isMergeCommit) {
+    bumpType = "minor";
 } else {
-    var version = process.argv[2].trim();
+    bumpType = "patch";
 }
 
-var dry = false;
-if (process.argv.length >= 3) {
-    dry = process.argv[3] === "dry";
+console.log(`Bump type: ${bumpType}`);
+console.log(`Current version: ${currentVersion}`);
+
+const newVersion = determineNewVersion();
+
+// Make sure the version is valid (semver)
+const versionRegex = /(\d+\.\d+)(.\d+)?(\-next\.[0-9]{3})?/;
+if (!versionRegex.test(newVersion)) {
+    console.error(`Invalid version format specified (semver expected, was ${newVersion})`);
+    process.exit(1);
 }
 
-if (version === "major" || version === "minor" || version === "patch") {
-    console.log(`Bumping version by ${version}`);
+console.log(`New version:     ${newVersion}`);
 
-    // Run git tag --sort=committerdate
-    var latestTag = child_process
-        .execSync("git ls-remote --tags --sort=-committerdate", { encoding: "utf8" })
-        .trim()
-        .split("\n")
-        .filter((tag) => !tag.includes("next"))[0];
+updateProjectVersions(newVersion);
 
-    var currentVersion = "0.0.0";
-    if (latestTag?.split("/")[2] !== undefined) {
-        currentVersion = latestTag.split("/")[2].replace("v", "");
+if (process.env.CI) {
+    pushNewTag(newVersion);
+}
+
+function determineNewVersion() {
+    let major = parseInt(currentVersion.split(".")[0]);
+    let minor = parseInt(currentVersion.split(".")[1]);
+    let patch = parseInt(currentVersion.split(".")[2]);
+
+    if (isPreRelease) {
+        const nextNumber =
+            tags
+                .filter((tag) => tag.startsWith(nextVersion))
+                .map((tag) => parseInt(tag.split("-next.")[1]))
+                .sort((a, b) => a - b)[0] + 1;
+
+        return `${nextVersion}.${nextNumber.toString().padStart(3, "0")}`;
     }
 
-    console.log(`Current version: '${currentVersion}'`);
-
-    var major = parseInt(currentVersion.split(".")[0]);
-    var minor = parseInt(currentVersion.split(".")[1]);
-    var patch = parseInt(currentVersion.split(".")[2]);
-
-    switch (version) {
+    switch (bumpType) {
         case "major":
             major++;
             minor = 0;
             patch = 0;
-            break;
+            return `${major}.${minor}.${patch}`;
         case "minor":
             minor++;
             patch = 0;
-            break;
+            return `${major}.${minor}.${patch}`;
         case "patch":
             patch++;
             break;
+        case "prerelease":
+            patch++;
+            return `${major}.${minor}.${patch}`;
     }
 
-    version = `${major}.${minor}.${patch}`;
-    tag = `v${version}`;
+    return `${major}.${minor}.${patch}`;
+}
 
-    if (branch == "next") {
-        tag += "-next";
+function updateProjectVersions(version: string) {
+    // Clean up version by removing a possible -next.001 suffix
+    version = version.replace(/-next\.[0-9]{3}/, "");
 
-        // Remove previous next tag
-        var alphaTags = child_process
-            .execSync("git tag --list", { encoding: "utf8" })
-            .trim()
-            .split("\n")
-            .filter((tag) => tag.startsWith(version));
+    console.log("Updating project versions to", version);
 
-        // Get the highest tag
-        var amountOfTags = alphaTags.map((tag) => parseInt(tag.split("-next.")[1]));
-        amountOfTags.sort((a, b) => a - b);
+    const packageJsons = glob.sync("**/package.json", { ignore: ["node_modules/**"] });
+    const cargoToml = glob.sync("**/Cargo.toml")[0];
+    const tauriConf = glob.sync("**/tauri.conf.json")[0];
 
-        var highestTag = 0;
-
-        if (amountOfTags.length > 0) {
-            highestTag = amountOfTags[amountOfTags.length - 1];
-        }
-
-        tag += ".";
-
-        // Format with three digits
-        tag += (highestTag + 1).toString().padStart(3, "0");
+    // Update the package.json file
+    for (const packageJson of packageJsons) {
+        console.log("Updating", packageJson);
+        let packageJsonContent = JSON.parse(readFileSync(packageJson, "utf8"));
+        packageJsonContent["version"] = version;
+        writeFileSync(packageJson, JSON.stringify(packageJsonContent, null, 4), "utf8");
     }
-} else {
-    var match = version.match(/(\d+\.\d+)(.\d+)(\-next)?/);
 
-    if (!match) {
-        console.error(`Invalid version format specified (semver expected, was ${version})`);
-        process.exit(1);
-    }
+    // Update the Cargo.toml file
+    console.log("Updating", cargoToml);
+    let cargoTomlContent = readFileSync(cargoToml, "utf8");
+    cargoTomlContent = cargoTomlContent.replace(/version = ".*"/, `version = "${version}"`);
+    writeFileSync(cargoToml, cargoTomlContent, "utf8");
+
+    // Update the tauri.conf.json file
+    console.log("Updating", tauriConf);
+    let tauriConfContent = JSON.parse(readFileSync(tauriConf, "utf8"));
+    tauriConfContent["package"]["version"] = version;
+    writeFileSync(tauriConf, JSON.stringify(tauriConfContent, null, 4), "utf8");
 }
 
-// Make sure the version is valid (semver)
-const versionRegex = /(\d+\.\d+)(.\d+)?(\-next)?/;
-if (!versionRegex.test(version)) {
-    console.error(`Invalid version format specified (semver expected, was ${version})`);
-    process.exit(1);
-}
+function pushNewTag(version: string) {
+    const tag = `${version}`;
 
-console.log(`New version: '${version}' (${tag})`);
-
-if (dry) process.exit(0);
-
-const packageJsons = glob.sync("**/package.json", { ignore: ["node_modules/**"] });
-const cargoToml = glob.sync("**/Cargo.toml")[0];
-const tauriConf = glob.sync("**/tauri.conf.json")[0];
-
-// Update the package.json file
-for (const packageJson of packageJsons) {
-    console.log(`Updating ${packageJson} to version ${version}`);
-    let packageJsonContent = JSON.parse(readFileSync(packageJson, "utf8"));
-    packageJsonContent["version"] = version;
-    writeFileSync(packageJson, JSON.stringify(packageJsonContent, null, 4), "utf8");
-}
-
-// Update the Cargo.toml file
-console.log(`Updating ${cargoToml} to version ${version}`);
-let cargoTomlContent = readFileSync(cargoToml, "utf8");
-cargoTomlContent = cargoTomlContent.replace(/version = ".*"/, `version = "${version}"`);
-writeFileSync(cargoToml, cargoTomlContent, "utf8");
-
-// Update the tauri.conf.json file
-console.log(`Updating ${tauriConf} to version ${version}`);
-let tauriConfContent = JSON.parse(readFileSync(tauriConf, "utf8"));
-tauriConfContent["package"]["version"] = version;
-writeFileSync(tauriConf, JSON.stringify(tauriConfContent, null, 4), "utf8");
-
-// Check if running in CI/CD
-if (process.env.CI) {
-    console.log("CI detected, pushing changes");
+    console.log("Pushing new tag", tag, "to remote");
 
     // Set git identity
     child_process.execSync(`git config user.email "41898282+github-actions[bot]@users.noreply.github.com"`, { encoding: "utf8" });
     child_process.execSync(`git config user.name "github-actions[bot]"`, { encoding: "utf8" });
 
     // Get latest commit author
-    var author = child_process.execSync("git log -1", { encoding: "utf8" }).trim().split("\n")[1].replace("Author: ", "");
+    let author = child_process.execSync("git log -1", { encoding: "utf8" }).trim().split("\n")[1].replace("Author: ", "");
 
     // Stage changes
     child_process.execSync("git add .", { encoding: "utf8" });
 
     // Commit, tag and push
-    var author_cmd = "";
+    let author_cmd = "";
     // Check if author is in valid format (Name <email>)
     if (/^.* <.*>$/.test(author)) {
         author_cmd = `--author="${author}"`;
@@ -165,7 +141,7 @@ if (process.env.CI) {
     const status = child_process.execSync("git status --porcelain", { encoding: "utf8" });
 
     if (status.trim() !== "") {
-        child_process.execSync(`git commit -m "🤖 version bump to ${tag}" ${author_cmd}`, { encoding: "utf8" });
+        child_process.execSync(`git commit -m "🤖 bump to ${tag}" ${author_cmd}`, { encoding: "utf8" });
     }
 
     child_process.execSync(`git tag ${tag}`, { encoding: "utf8" });
