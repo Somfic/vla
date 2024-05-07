79167f8f838235229e08928c68e899de2094691a
Lucas <lucas@somfic.dev>
2024-05-07 17:12:07 +0200

    show window when dom content has loaded

--- src/ui/src/routes/+page.svelte
+++ src/ui/src/routes/+page.svelte
@@ -2,24 +2,9 @@
 	import { invoke } from '@tauri-apps/api/tauri';
 	import { onMount } from 'svelte';
 
-	let date = new Date();
-	let plugin: any = null;
-
-	onMount(async () => {
-		const plugin_sources = (await invoke('get_all_plugins')) as string[];
-		const plugin_source = plugin_sources[0];
-
-		plugin = await import(plugin_source);
-
-		console.log(plugin);
+	onMount(() => {
+		invoke('show_window');
 	});
-
-	setInterval(() => {
-		date = new Date();
-	}, 10);
 </script>
 
-<main>
-	<p>plugins: {JSON.stringify(plugin)}</p>
-	<p>Hello {date}</p>
 </main>
