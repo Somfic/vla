5862bf349d321b363fe7e4464ef6aa21998045c3
Lucas <lucas@somfic.dev>
2024-05-08 01:18:41 +0200

    moved layout to `+layout.svelte`

---
+++ src/ui/src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+	import { onMount } from 'svelte';
+	import '../styles/global.scss';
+	import TitleBar from '$lib/layout/TitleBar.svelte';
+	import StatusBar from '$lib/layout/StatusBar.svelte';
+
+	onMount(() => {
+		invoke('show_window');
+	});
+</script>
+
+<main id="application">
+	<TitleBar />
+	<div id="content">
+		<slot />
+	</div>
+	<StatusBar />
+</main>
+
+<style lang="scss">
+	#application {
+		max-width: 100vw;
+		display: flex;
+	}
+
+	#content {
+		flex-grow: 1;
+	}
+</style>
