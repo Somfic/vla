94decb35554b29eb10d5d00eff97127f7ddee1e1
Lucas <lucas@somfic.dev>
2024-05-08 01:18:09 +0200

    added status bar

---
+++ src/ui/src/lib/layout/StatusBar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<div class="status-bar">
+	<div class="ribbon">
+		<p>test</p>
+		<p>test</p>
+	</div>
+	<div class="ribbon"><p>test</p></div>
+</div>
+
+<style lang="scss">
+	.status-bar {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 10px;
+	}
+
+	.ribbon {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: rgba(30, 30, 30, 0.8);
+		border: 1px solid rgba(255, 255, 255, 0.1);
+		padding: 5px 15px;
+		border-radius: 10px;
+		gap: 30px;
+		color: #8e8e8d;
+	}
+</style>
