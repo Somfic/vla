98360f140a516d554d4a99a7d457405b9ed60e77
Lucas <lucas@somfic.dev>
2024-05-08 19:22:06 +0200

    declutter main page

--- src/ui/src/routes/+page.svelte
+++ src/ui/src/routes/+page.svelte
@@ -4,14 +4,6 @@
 
 <div class="content-wrapper">
 	<Ribbon />
-	<div class="content">
-		<h1>Hello</h1>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, mollitia quod. Perspiciatis,
-			laboriosam suscipit! Quam excepturi quae itaque architecto dolore doloremque error, porro
-			ratione necessitatibus eum a ullam blanditiis repellat.
-		</p>
-	</div>
 </div>
 
 <style lang="scss">
