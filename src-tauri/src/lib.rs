47a69c62a831f5f48ba51bd9aa12a0b6ba4285b7
Lucas <lucas@somfic.dev>
2025-09-14 12:09:56 +0200

    Integrate taurpc for Rust/TypeScript IPC

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,14 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+#[taurpc::procedures(export_to = "../src/lib/core_gen.ts")]
+pub trait Api {
+    async fn hello_world(name: String) -> String;
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+#[derive(Clone)]
+pub struct ApiImpl;
+
+#[taurpc::resolvers]
+impl Api for ApiImpl {
+    async fn hello_world(self, name: String) -> String {
+        format!("Hello, {}! You've been greeted from Rust!", name)
+    }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use vla_lib::Api;
 
 fn main() {
-    vla_lib::run()
+    run()
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(taurpc::create_ipc_handler(vla_lib::ApiImpl.into_handler()))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
