430818edb026cb4011ea1b2d634e59fe62e62a46
Lucas <lucas@somfic.dev>
2025-09-24 23:10:14 +0200

    🐛 create graph file if it not yet exists
    semver: patch

--- ui/lib/core.ts
+++ ui/lib/core.ts
@@ -26,10 +26,9 @@ export type NodeData = { brick_id: string; brick: Brick | null; arguments: Parti
 
 export type Point = { x: number; y: number }
 
-const ARGS_MAP = { '':'{"get_brick":["brick_id"],"get_bricks":[],"hello_world":["name"],"load_graph":["filename"],"save_graph":["graph","filename"]}' }
+const ARGS_MAP = { '':'{"get_brick":["brick_id"],"get_bricks":[],"load_graph":["filename"],"save_graph":["graph","filename"]}' }
 export type Router = { "": {get_brick: (brickId: string) => Promise<Brick | null>, 
 get_bricks: () => Promise<Brick[]>, 
-hello_world: (name: string) => Promise<string>, 
 load_graph: (filename: string) => Promise<Graph>, 
 save_graph: (graph: Graph, filename: string) => Promise<string>} };
 
