1d7be065ca3d2c2c7cfb2419463da05745d9ca1f
Lucas <lucas@somfic.dev>
2025-09-27 19:26:17 +0200

    🧑‍💻 add nix development files

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
-  description = "VLA - A Tauri App with SvelteKit frontend";
+  description = "Development environment for the Som programming language";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,101 +14,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.rust-bin.stable."1.89.0".default.override {
+          extensions =
+            [ "rust-src" "rust-analyzer" "clippy" "llvm-tools-preview" ];
         };
-
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rustfmt" "clippy" ];
-        };
-
-        libraries = with pkgs; [
-          webkitgtk_4_1
-          gtk3
-          cairo
-          gdk-pixbuf
-          glib
-          dbus
-          openssl_3
-          librsvg
-          libsoup_3
-          pango
-          atk
-          at-spi2-atk
-          harfbuzz
-        ];
-
-        packages = with pkgs; [
-          rustToolchain
-          cargo-tauri
-          bun
-          pkg-config
-          gobject-introspection
-          git
-          curl
-          wget
-          dbus
-          openssl_3
-          glib
-          gtk3
-          libsoup_3
-          webkitgtk_4_1
-          librsvg
-          pango
-          atk
-          at-spi2-atk
-          harfbuzz
-        ];
-
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = packages;
-          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            cargo-llvm-cov
+            just
+            cargo-edit
+            cargo-watch
+            cargo-deny
+            llvm
+            ripgrep
+            fd
+            bat
+            bun
+          ];
 
-          shellHook = ''
-            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-            export WEBKIT_DISABLE_COMPOSITING_MODE=1
-            export RUST_BACKTRACE=1
-            
-            # Install frontend dependencies if they don't exist
-            if [ ! -d "node_modules" ]; then
-              echo "Installing frontend dependencies..."
-              bun install
-            fi
-          '';
-
-          # Environment variables for Tauri
-          WEBKIT_DISABLE_COMPOSITING_MODE = "1";
           RUST_BACKTRACE = "1";
+          RUST_LOG = "debug";
         };
 
-        # Package for building the application
-        packages.default = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "vla";
-          version = "0.1.0";
-
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./core/Cargo.lock;
-          };
-
-          nativeBuildInputs = packages;
-          buildInputs = libraries;
-
-          meta = with pkgs.lib; {
-            description = "VLA - A Tauri App";
-            homepage = "https://github.com/your-username/vla";
-            license = licenses.mit;
-            maintainers = [ ];
-            platforms = platforms.linux ++ platforms.darwin;
-          };
-        };
-
-        # Formatter for the flake
         formatter = pkgs.nixpkgs-fmt;
       });
 }
