ec19630b089860ce865588e3792ba377982049ef
Lucas <lucas@somfic.dev>
2025-10-04 01:11:09 +0200

    🩹 fix cargo docs test failing
    semver: patch

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,26 +3,6 @@ pub mod bricks;
 pub mod canvas;
 pub mod prelude;
 
-/// Macro to trigger execution outputs from within brick functions
-///
-/// Usage: `trigger!("output_id");`
-///
-/// This macro marks an execution output for triggering during brick execution.
-/// The ExecutionEngine will collect these triggers after the brick function
-/// completes and send execution signals to connected downstream nodes.
-///
-/// # Examples
-///
-/// ```rust
-/// use vla::trigger;
-///
-/// // In an if/else brick
-/// if condition {
-///     trigger!("true_branch");
-/// } else {
-///     trigger!("false_branch");
-/// }
-/// ```
 #[macro_export]
 macro_rules! trigger {
     ($output_id:expr) => {
